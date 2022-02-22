91feaf42414ffc88af31931efa5f634c38d879b5
Stella <stellax212@gmail.com>
2022-02-22 14:51:37 +0100

    feat/homepage

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>WriteByMe</title>
+        <link rel="icon" href="/favicon-32x32.png" />
       </Head>
 
       <main>
